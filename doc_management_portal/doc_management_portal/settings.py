134b2bcb33adb77c0efe40b2fd279d61eae8dfbe
Lawrence Sharma <che3zcake@gmail.com>
2025-05-28 22:18:37 +0530

    changes cors

--- doc_management_portal/doc_management_portal/settings.py
+++ doc_management_portal/doc_management_portal/settings.py
@@ -140,6 +140,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://doc-ai-protal-1.vercel.app",
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
